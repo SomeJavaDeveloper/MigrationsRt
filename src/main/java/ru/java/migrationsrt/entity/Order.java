eb67d00f9c1548d2294a97038076490534f467fd
Artyom <divinefridaay@yandex.ru>
2022-11-12 23:43:04 +0300

    Optimized imports

--- src/main/java/ru/java/migrationsrt/entity/Order.java
+++ src/main/java/ru/java/migrationsrt/entity/Order.java
@@ -1,9 +1,21 @@
 package ru.java.migrationsrt.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Getter
 @Setter
